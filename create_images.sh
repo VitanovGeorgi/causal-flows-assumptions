d785c04d77c53b4dbfa68266c935196bd0be1171
George <vitanovgeorgi.gv@gmail.com>
2024-05-05 13:18:28 +0200

    matplotlib repaced with seaborn for plotting; issue with sending mse and getting results for rmse due to the filter resolved

--- create_images.sh
+++ create_images.sh
@@ -2,9 +2,9 @@
 
 declare -a arr=('kl_distance' 'log_prob' 
     'log_prob_p' 'log_prob_true' 'loss' 'mmd_int_x1' 
-    'mmd_int_x2' 'mmd_int_x3' 'mmd_obs' 'mse_cf_x1' 
-    'mse_cf_x2' 'mse_cf_x3'
-    'rmse_ate_x1=25_50' 'rmse_ate_x1=25_75' 'rmse_ate_x1=50_75' 
+    'mmd_int_x2' 'mmd_int_x3' 'mmd_obs' 
+    'mse_cf_x1' 'mse_cf_x2' 'mse_cf_x3'
+    'rmse_ate_x1' 'rmse_ate_x2' 'rmse_ate_x3' 
     'rmse_cf_x1' 'rmse_cf_x2' 'rmse_cf_x3')
 
 
@@ -14,7 +14,7 @@ for metric in "${arr[@]}"
     python plot_aggregated_results.py \
     --source=output_causal_nf/aggregated_results/results_chain \
      --metric $metric \
-      --affected_var 1,2 
+    #   --affected_var 2,3 
 
 
 done
